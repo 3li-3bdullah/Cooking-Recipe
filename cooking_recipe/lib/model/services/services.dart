ed690e11ec3d1ff179f9c4e256b9feada18ea062
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-07 17:33:12 +0200

    Created new folder & file and added api url

---
+++ cooking_recipe/lib/model/services/services.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+class Services {
+  final url = 'https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=3&calories=591-722&health=alcohol-free';
+   getApiData() async {
+     
+   }
+}
