17cd1660de1035db33ad3759e83b996462f8743a
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-07 16:42:40 +0200

    Added textfield and some features

--- cooking_recipe/lib/view/home.dart
+++ cooking_recipe/lib/view/home.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-
-
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: const Text('Cooking Recipe'),)
-      
+        appBar: AppBar(
+          elevation: 0,
+      title: const Text('Cooking Recipe'),
+    ),
+    body: Column(children:[
+      TextField(decoration: InputDecoration(
+        border : OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        filled: true,
+        fillColor: Colors.green.withOpacity(0.2),
+      ),)
+    ])
     );
   }
 }
