213a85713afc079434d452c268d27e29d53a951c
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-07 18:58:04 +0200

    Added sizedbox/getx/gridview and some codes inside them

--- cooking_recipe/lib/view/home.dart
+++ cooking_recipe/lib/view/home.dart
@@ -1,25 +1,48 @@
+import 'package:cooking_recipe/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends GetWidget<HomeViewModel> {
+  Home({Key? key}) : super(key: key);
+  HomeViewModel xcontroller = Get.put(HomeViewModel());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
-      title: const Text('Cooking Recipe'),
-    ),
-    body: Container(
-      margin: const EdgeInsets.symmetric(horizontal:20,vertical:20),
-      child: Column(children:[
-        TextField(decoration: InputDecoration(
-          border : OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-          filled: true,
-          fillColor: Colors.green.withOpacity(0.4),
-        ),)
-      ]),
-    )
-    );
+          title: const Text('Cooking Recipe'),
+        ),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            TextField(
+              decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                filled: true,
+                fillColor: Colors.green.withOpacity(0.4),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GetX<HomeViewModel>(
+              builder: (controller) => GridView.builder(
+                physics: const ScrollPhysics(),
+                shrinkWrap: true,
+                primary: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5),
+                  itemCount: controller.list.length,
+                  itemBuilder: (context, i) {
+                    final lengthData = controller.list[i];
+                    return Column(children: [Card(child: Image.network(lengthData.image.toString()))]);
+                  }),
+            )
+          ]),
+        ));
   }
 }
