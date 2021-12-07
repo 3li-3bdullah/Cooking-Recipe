3b65a6553e9a29668688078105aabc97fd4a907f
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-07 22:20:50 +0200

    Created method to fetch data

--- cooking_recipe/lib/model/services/services.dart
+++ cooking_recipe/lib/model/services/services.dart
@@ -3,16 +3,17 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Services {
-  final url =
+  static String url =
       'https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=3&calories=591-722&health=alcohol-free';
-  getApiData() async {
+ static getApiData() async {
     var response = await http.get(Uri.parse(url));
     Map json = jsonDecode(response.body);
     json['hits'].forEach((e) {
       Product product = Product(
         url : e['recipe']['uri'],
         image: e['recipe']['image'],
-        source: e['recipe']['source']
+        source: e['recipe']['source'],
+        label : e['recipe']['label']
       );
     });
   }
