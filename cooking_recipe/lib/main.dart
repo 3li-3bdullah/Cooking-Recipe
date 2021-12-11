7542a45cc921a24d33ac6e70de98fb441c5d6f82
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-12 01:36:19 +0200

    Removed unncessary comment

--- cooking_recipe/lib/main.dart
+++ cooking_recipe/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cooking_recipe/view/home.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Cooking Recipe',
-      theme: ThemeData(
-      ),
       home: Home(),
     );
   }
