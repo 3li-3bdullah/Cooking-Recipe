5651affd0414684a60c486b0c371dc4ba4d79ab0
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-12 23:54:33 +0200

    Added singlechildscrollview&column and wrap the listview.builder with sizedbox

--- cooking_recipe/lib/view/home.dart
+++ cooking_recipe/lib/view/home.dart
@@ -17,58 +17,67 @@ class Home extends GetWidget<HomeViewModel> {
         elevation: 0,
         title: const Text('Cooking Recipe'),
       ),
-      body: FutureBuilder(
-          future: controller.fetchData(),
-          builder: (context, AsyncSnapshot<Model> snapshot) {
-            Model? data = snapshot.data;
-            if (snapshot.hasData) {
-              return GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10),
-                  itemCount: data!.hits!.length,
-                  itemBuilder: (context, i) {
-                    return InkWell(
-                      onTap: () {},
-                      child: Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    data.hits![i].image.toString()),
-                                fit: BoxFit.fill)),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(3),
-                                height: 40,
-                                color: Colors.black.withOpacity(0.5),
-                                child: Center(
-                                  child: Text(
-                                    data.hits![i].label.toString(),
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(3),
-                                height: 40,
-                                color: Colors.black.withOpacity(0.5),
-                                child: Center(
-                                  child: Text(
-                                    "Source : " +
-                                        data.hits![i].source.toString(),
-                                  ),
-                                ),
-                              )
-                            ]),
-                      ),
-                    );
-                  });
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
+      body: SingleChildScrollView(
+      child: Column(
+        children: [
+         
+          FutureBuilder(
+              future: controller.fetchData(),
+              builder: (context, AsyncSnapshot<Model> snapshot) {
+                Model? data = snapshot.data;
+                if (snapshot.hasData) {
+                  return SizedBox(
+                    height: size.height,
+                    child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 10),
+                        itemCount: data!.hits!.length,
+                        itemBuilder: (context, i) {
+                          return InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          data.hits![i].image.toString()),
+                                      fit: BoxFit.fill)),
+                              child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.all(3),
+                                      height: 40,
+                                      color: Colors.black.withOpacity(0.5),
+                                      child: Center(
+                                        child: Text(
+                                          data.hits![i].label.toString(),
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.all(3),
+                                      height: 40,
+                                      color: Colors.black.withOpacity(0.5),
+                                      child: Center(
+                                        child: Text(
+                                          "Source : " +
+                                              data.hits![i].source.toString(),
+                                        ),
+                                      ),
+                                    )
+                                  ]),
+                            ),
+                          );
+                        }),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              }),
     );
   }
 }
