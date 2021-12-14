758a80f4a1198f0cf62fb7663942666de59a1565
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-14 09:59:43 +0200

    seperated url so i can call it in anywhere

--- cooking_recipe/lib/view_model/home_view_model.dart
+++ cooking_recipe/lib/view_model/home_view_model.dart
@@ -4,16 +4,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class HomeViewModel extends GetxController {
-
   @override
   void onInit() {
     super.onInit();
     fetchData();
   }
+
+  final Uri url = Uri.parse("https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free");
   Future<Model> fetchData() async {
-    final response = await http.get(Uri.parse(
-        "https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free"));
-       
+    final response = await http.get(url);
+
     if (response.statusCode == 200) {
       try {
         Get.snackbar('Cooking Recipe', 'Have a beautiful day .');
@@ -21,6 +21,6 @@ class HomeViewModel extends GetxController {
         Get.snackbar('Errors', e.toString());
       }
     }
-     return Model.fromJson(jsonDecode(response.body));
+    return Model.fromJson(jsonDecode(response.body));
   }
 }
