0115c267532805f406fdc62d140da98f8a0c6efb
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-14 10:15:34 +0200

    created instance var

--- cooking_recipe/lib/view_model/home_view_model.dart
+++ cooking_recipe/lib/view_model/home_view_model.dart
@@ -10,6 +10,7 @@ class HomeViewModel extends GetxController {
     fetchData();
   }
 
+  late final String text;
   final Uri url = Uri.parse("https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free");
   Future<Model> fetchData() async {
     final response = await http.get(url);
