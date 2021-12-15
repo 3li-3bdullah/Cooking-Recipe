ae1d7f90ed7b374d1097a6d146e436947f12a3dc
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-15 23:23:14 +0200

    removed the onInit and final

--- cooking_recipe/lib/view_model/search_page_view_model.dart
+++ cooking_recipe/lib/view_model/search_page_view_model.dart
@@ -4,20 +4,13 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class SearchPageViewModel extends GetxController {
-  @override
-  void onInit() {
-    super.onInit();
-    fetchData(cSearch!);
-  }
-
-  String? cSearch;
   late String url;
 
   Future<Model> fetchData(String cSearch) async {
     url =
         "https://api.edamam.com/search?q=$cSearch&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free";
-    
-    final response = await http.get(Uri.parse(url));
+
+     http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       try {
