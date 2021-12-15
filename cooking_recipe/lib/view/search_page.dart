9d7c78813c1caafd9670f502349473bff2208867
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-15 15:00:51 +0200

    Removed unnecessary widgets

--- cooking_recipe/lib/view/search_page.dart
+++ cooking_recipe/lib/view/search_page.dart
@@ -1,10 +1,13 @@
 import 'package:cooking_recipe/model/product.dart';
-import 'package:cooking_recipe/view_model/home_view_model.dart';
+import 'package:cooking_recipe/view/details_page.dart';
+import 'package:cooking_recipe/view_model/search_page_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SearchPage extends GetWidget<HomeViewModel> {
-  // final controller = Get.lazyPut(() => HomeViewModel());
+class SearchPage extends GetWidget<SearchPageViewModel> {
+  
+  SearchPageViewModel scontroller = Get.put(SearchPageViewModel());
+
   String? search;
   SearchPage({this.search});
 
@@ -16,67 +19,64 @@ class SearchPage extends GetWidget<HomeViewModel> {
           elevation: 0,
           title: const Text('Cooking Recipe'),
         ),
-        body: SingleChildScrollView(
-            child: Column(children: [
-          FutureBuilder(
-              future: controller.fetchData(),
-              builder: (context, AsyncSnapshot<Model> snapshot) {
-                Model? data = snapshot.data;
-                if (snapshot.hasData) {
-                  return SizedBox(
-                    height: size.height,
-                    child: GridView.builder(
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                crossAxisSpacing: 10,
-                                mainAxisSpacing: 10),
-                        itemCount: data!.hits!.length,
-                        itemBuilder: (context, i) {
-                          return InkWell(
-                            onTap: () {
-                              Get.to(() => DetailsPage(url: controller.url));
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: NetworkImage(
-                                          data.hits![i].image.toString()),
-                                      fit: BoxFit.fill)),
-                              child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Container(
-                                      padding: const EdgeInsets.all(3),
-                                      height: 40,
-                                      color: Colors.black.withOpacity(0.5),
-                                      child: Center(
-                                        child: Text(
-                                          data.hits![i].label.toString(),
-                                        ),
+        body: FutureBuilder(
+            future: controller.fetchData(search!),
+            builder: (context, AsyncSnapshot<Model> snapshot) {
+              Model? data = snapshot.data;
+              if (snapshot.hasData) {
+                return SizedBox(
+                  height: size.height,
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10),
+                      itemCount: data!.hits!.length,
+                      itemBuilder: (context, i) {
+                        return InkWell(
+                          onTap: () {
+                            Get.to(() => DetailsPage(url: data.hits![i].url.toString()));
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        data.hits![i].image.toString()),
+                                    fit: BoxFit.fill)),
+                            child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(3),
+                                    height: 40,
+                                    color: Colors.black.withOpacity(0.5),
+                                    child: Center(
+                                      child: Text(
+                                        data.hits![i].label.toString(),
                                       ),
                                     ),
-                                    Container(
-                                      padding: const EdgeInsets.all(3),
-                                      height: 40,
-                                      color: Colors.black.withOpacity(0.5),
-                                      child: Center(
-                                        child: Text(
-                                          "Source : " +
-                                              data.hits![i].source.toString(),
-                                        ),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.all(3),
+                                    height: 40,
+                                    color: Colors.black.withOpacity(0.5),
+                                    child: Center(
+                                      child: Text(
+                                        "Source : " +
+                                            data.hits![i].source.toString(),
                                       ),
-                                    )
-                                  ]),
-                            ),
-                          );
-                        }),
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              }),
-        ])));
+                                    ),
+                                  )
+                                ]),
+                          ),
+                        );
+                      }),
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
