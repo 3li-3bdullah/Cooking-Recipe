0ee063be1a0ac30ec5a5698b968e1f3c2938ab91
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-18 23:00:35 +0200

    Added debugShowCheckedModeBanner:false

--- cooking_recipe/lib/main.dart
+++ cooking_recipe/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       title: 'Cooking Recipe',
       home: Home(),
