7ba7eadd33be6f1620d756ef10ea5cde0379dfa7
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-18 17:18:05 +0200

    Added titletext

--- cooking_recipe/lib/view_model/home_view_model.dart
+++ cooking_recipe/lib/view_model/home_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:cooking_recipe/model/product.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -10,14 +11,24 @@ class HomeViewModel extends GetxController {
     fetchData();
   }
 
-    RxString text = "chicken".obs;
-  
+  RxString text = "chicken".obs;
+
   Future<Model> fetchData() async {
-    final response = await http.get(Uri.parse("https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free"));
+    final response = await http.get(Uri.parse(
+        "https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free"));
 
     if (response.statusCode == 200) {
       try {
-        Get.snackbar('Cooking Recipes', 'Good luck and wish you enjoy with most beautiful cooking recipes.');
+        Get.snackbar('Cooking Recipes', 'Good luck',
+            titleText: Text(
+              'Cooking Recipes',
+              style: TextStyle(
+                  color: Colors.orange.shade400,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            
+            );
       } catch (e) {
         Get.snackbar('Errors', e.toString());
       }
