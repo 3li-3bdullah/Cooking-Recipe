2f9c814ccce5ace2b9e1d58abb524731617bac4b
Ali Abullah <PROGRMMER7@gmail.com>
2021-12-20 21:19:44 +0200

    Removed if condition and status code

--- cooking_recipe/lib/view_model/home_view_model.dart
+++ cooking_recipe/lib/view_model/home_view_model.dart
@@ -12,33 +12,11 @@ class HomeViewModel extends GetxController {
   }
 
   RxString text = "chicken".obs;
-  RxBool mode =  true.obs;
+  RxBool mode = true.obs;
 
   Future<Model> fetchData() async {
     final response = await http.get(Uri.parse(
         "https://api.edamam.com/search?q=chicken&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free"));
-
-    if (response.statusCode == 200) {
-      try {
-        Get.snackbar(
-            'Cooking Recipes', 'Look for your favorite food to make it.',
-            titleText: const Text(
-              'Cooking Recipes',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold),
-            ),colorText: Colors.orange.shade400,
-            messageText:Text('Look for your favorite food to make it.'),
-           icon: IconButton(
-          onPressed: () {},
-          icon: CircleAvatar(backgroundImage: AssetImage('assets/images/snack.jpg'),),
-        )
-            );
-        
-      } catch (e) {
-        Get.snackbar('Errors', e.toString());
-      }
-    }
     return Model.fromJson(jsonDecode(response.body));
   }
 }
