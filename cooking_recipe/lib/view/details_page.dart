63d672ae962badbbe8c022465706b6ac013ba70e
3li-3bdullah <PROGRMMER7@gmail.com>
2022-01-05 23:33:01 +0200

    Added comment it's ignore: must_be_immutabl

--- cooking_recipe/lib/view/details_page.dart
+++ cooking_recipe/lib/view/details_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 
+// ignore: must_be_immutable
 class DetailsPage extends StatelessWidget {
  
   String url;
