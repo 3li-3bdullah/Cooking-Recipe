df53624246c740c17472ce0c4b625a09e4b4748e
3li-3bdullah <PROGRMMER7@gmail.com>
2022-01-05 23:30:04 +0200

    Added initial binding

--- cooking_recipe/lib/main.dart
+++ cooking_recipe/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cooking_recipe/binding/my_binding.dart';
 import 'package:cooking_recipe/view/home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      initialBinding: MyBinding(),
       theme: ThemeData.dark(),
       title: 'Cooking Recipe',
       home: Home(),
