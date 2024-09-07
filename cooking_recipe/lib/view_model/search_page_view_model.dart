6f8dde934987cea8469e88e4fe60cc98db6c8cf5
3li-3bdullah <PROGRMMER7@gmail.com>
2024-09-07 12:04:28 +0200

    changed the app id and api key

--- cooking_recipe/lib/view_model/search_page_view_model.dart
+++ cooking_recipe/lib/view_model/search_page_view_model.dart
@@ -9,7 +9,7 @@ class SearchPageViewModel extends GetxController {
 
   Future<Model> fetchData(String cSearch) async {
     url =
-        "https://api.edamam.com/search?q=$cSearch&app_id=1bd5ac26&app_key=9cdd5f2c583a0ea0f1ad4a95773bc8f3&from=0&to=100&calories=591-722&health=alcohol-free";
+        "https://api.edamam.com/search?q=$cSearch&app_id=91919746&app_key=1ffacc6123a4c1ab558945b4963d2dff&from=0&to=100&calories=591-722&health=alcohol-free";
 
      http.Response response = await http.get(Uri.parse(url));
 
